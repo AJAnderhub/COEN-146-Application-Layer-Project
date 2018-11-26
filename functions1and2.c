77bdd4d2741b0754a14b5ca0f42211a59d32274e
jack dishman <me@jackdishman.com>
2018-11-25 19:40:30 -0800

    Add files via upload
    Added Functions 1 and 2
    
    Function 1:
    -Request names of all files in server, by sending packet type 1
    -Wait for response from server - request type 2
    -Display received names on stdout
    
    Function 2:
    -Send file names - request type 2

--- functions1and2.c
+++ functions1and2.c
@@ -1,10 +1,13 @@
 /*
-* Name: TBD
+* Name: Jack Dishman
 * Title: Final Project Functions 1 and 2
-* Description: TODO
+* Description: Request and send the names of all files in directory "/Files"
 */
 #include "header.h"
-
+#include <stdio.h>
+#include <sys/socket.h>
+#include <dirent.h>
+#include <string.h>
 
 
 /*** Request all filenames ***
@@ -15,7 +18,15 @@
 
 */
 void SendRequest1(int sockfd){
-    // TODO
+
+	struct Packet req1;
+	req1.request_type=1;
+	size_t size = sizeof(req1);
+	send(sockfd,&req1, size,0);
+	while(recv(sockfd,&req1,sizeof(struct Packet),0) != 0) {
+		printf("%s\n", req1.fileName);
+	}
+
 }
 
 /*** Send all filenames ***
@@ -25,5 +36,26 @@ void SendRequest1(int sockfd){
 
 */
 void SendRequest2(int connectSockfd){
-    // TODO
+
+	struct Packet req2;
+	req2.request_type = 2;
+  struct dirent *de;  // Pointer for directory entry
+  DIR *dr = opendir("./Files");
+  if (dr == NULL)
+  {
+    req2.error = 1;
+  }else{
+  	req2.error = 0;
+  }
+  //read file names in directory
+  while ((de = readdir(dr)) != NULL){
+    memset(req2.fileName, '\0', sizeof(req2.fileName));
+    for (int i = 0; i < MAX_FILENAME_LENGTH; i++){
+    	req2.fileName[i] = de->d_name[i];
+    }
+    send(connectSockfd,&req2,sizeof(struct Packet),0);
+  }
+
+	closedir(dr);
+
 }
